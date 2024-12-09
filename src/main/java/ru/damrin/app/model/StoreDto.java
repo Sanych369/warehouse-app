9042090778f3ec7d393748b9bd6b0037d9564131
Sanych369 <killa0007@mail.ru>
2024-12-09 13:28:49 +0300

    Расширение ДТО для времени приёмки/списания

--- src/main/java/ru/damrin/app/model/StoreDto.java
+++ src/main/java/ru/damrin/app/model/StoreDto.java
@@ -1,10 +1,11 @@
 package ru.damrin.app.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * DTO получения информации о складе.
  */
 public record StoreDto(String goodName, BigDecimal purchasePrice, Long arrivedTotal, Long consumptionTotal,
-                       String reason) {
+                       String reason, LocalDateTime createdAt) {
 }
